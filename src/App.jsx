e7da29b096df0327b2af80d79b102ca5d206b657
ahsanmughal11 <sn092702@gmail.com>
2026-06-06 19:21:07 +0500

    firebase connected

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
+import app from './firebase/firebaseConfig'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  console.log("Firebase app initialized:", app)
 
   return (
     <>
-      <h1>Vite + React</h1>
+      <div className="min-h-screen flex items-center justify-center">
+        <h1 className="text-2xl font-bold text-green-600">
+          Firebase Connected 🚀
+        </h1>
+      </div>
     </>
   )
 }
